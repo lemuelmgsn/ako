b40478ee3eb9919b9d592fa218781b3e07b40244
lemuelmgsn <magsinolemuel68@gmail.com>
2025-02-28 14:05:54 +0100

    code: HTML opgezet voor pagina

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<header>
+    <h1>Lemuel Magsino</h1>
+    <nav>
+        <ul>
+            <li>About</li>
+            <li>Experiences</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+
+<section>
+    <p>Hello, i'm</p>
+    <h2>Lemuel Magsino</h2>
+    <h3>Frontend developer</h3>
+
+    <div>
+        <a href="#CV">Download CV</a>
+        <a href="#Contact">Contact info</a>
+    </div>
+
+</section>
+
+<picture>
+
+</picture>
+
+
+
+</main>
